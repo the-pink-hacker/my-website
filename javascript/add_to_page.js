897bff93229e3235af673d66ca6b8eee4357e6d5
Ryan Garrett <RyanGar46@gmail.com>
2021-11-12 00:36:35 -0500

    Dropdown menu

--- javascript/add_to_page.js
+++ javascript/add_to_page.js
@@ -7,6 +7,18 @@ function AddNavBar() {
 				<a href="blender.html">Blender</a>
 				<a href="other.html">Other</a>
 				<a href="links.html">Links</a>
+				<div>
+					<div class="dropdown">Projects</div>
+					<div class="dropdown_content">
+						<a href="">Classic 3D</a>
+						<a href="">Classic 3D - Lite</a>
+						<a href="">Classic 3D - Logs</a>
+						<a href="">Classic 3D - Mod Support</a>
+						<hr>
+						<a href="">Items+</a>
+						<a href="">No Armor</a>
+					</div>
+				</div>
 			</div>
 		</div>
 	`);
