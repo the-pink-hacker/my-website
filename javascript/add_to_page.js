b67c002faae8d4d8084c498662164d1357da98df
Ryan Garrett <RyanGar46@gmail.com>
2021-11-14 12:52:40 -0500

    Improve projects

--- javascript/add_to_page.js
+++ javascript/add_to_page.js
@@ -2,23 +2,22 @@ function AddNavBar() {
 	$("body").prepend(`
 		<div class="navbar" id="navbar">
 			<div class="navbar_links">
-				<a href="index.html">Main</a>
-				<a href="curseforge.html">CurseForge</a>
-				<a href="blender.html">Blender</a>
-				<a href="other.html">Other</a>
-				<a href="links.html">Links</a>
-				<div>
-					<div class="dropdown">Projects</div>
+				<a href="/index.html">Main</a>
+				<div class="dropdown_container">
+					<a class="dropdown" href="/projects.html">Projects</a>
 					<div class="dropdown_content">
-						<a href="">Classic 3D</a>
-						<a href="">Classic 3D - Lite</a>
-						<a href="">Classic 3D - Logs</a>
-						<a href="">Classic 3D - Mod Support</a>
+						<a href="/projects/classic_3d.html">Classic 3D</a>
+						<a href="/projects/classic_3d_lite.html">Classic 3D - Lite</a>
+						<a href="/projects/classic_3d_logs.html">Classic 3D - Logs</a>
+						<a href="/projects/classic_3d_mod.html">Classic 3D - Mod Support</a>
 						<hr>
-						<a href="">Items+</a>
-						<a href="">No Armor</a>
+						<a href="/projects/items_plus.html">Items+</a>
+						<a href="/projects/no_armor.html">No Armor</a>
 					</div>
 				</div>
+				<a href="/blender.html">Blender</a>
+				<a href="/other.html">Other</a>
+				<a href="/links.html">Links</a>
 			</div>
 		</div>
 	`);
