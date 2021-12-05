a7d34661b07667347d6a886d412098bdd88961ef
Ryan Garrett <RyanGar46@gmail.com>
2021-12-05 01:35:36 -0500

    Fixed links

--- javascript/add_to_page.js
+++ javascript/add_to_page.js
@@ -2,22 +2,22 @@ function AddNavBar() {
 	$("body").prepend(`
 		<div class="navbar" id="navbar">
 			<div class="navbar_links">
-				<a href="/index.html">Main</a>
+				<a href="/my-website/index.html">Main</a>
 				<div class="dropdown_container">
-					<a class="dropdown" href="/projects.html">Projects</a>
+					<a class="dropdown" href="/my-website/projects.html">Projects</a>
 					<div class="dropdown_content">
-						<a href="/projects/classic_3d.html">Classic 3D</a>
-						<a href="/projects/classic_3d_lite.html">Classic 3D - Lite</a>
-						<a href="/projects/classic_3d_logs.html">Classic 3D - Logs</a>
-						<a href="/projects/classic_3d_mod.html">Classic 3D - Mod Support</a>
+						<a href="/my-website/projects/classic_3d.html">Classic 3D</a>
+						<a href="/my-website/projects/classic_3d_lite.html">Classic 3D - Lite</a>
+						<a href="/my-website/projects/classic_3d_logs.html">Classic 3D - Logs</a>
+						<a href="/my-website/projects/classic_3d_mod.html">Classic 3D - Mod Support</a>
 						<hr>
-						<a href="/projects/items_plus.html">Items+</a>
-						<a href="/projects/no_armor.html">No Armor</a>
+						<a href="/my-website/projects/items_plus.html">Items+</a>
+						<a href="/my-website/projects/no_armor.html">No Armor</a>
 					</div>
 				</div>
-				<a href="/blender.html">Blender</a>
-				<a href="/other.html">Other</a>
-				<a href="/links.html">Links</a>
+				<a href="/my-website/blender.html">Blender</a>
+				<a href="/my-website/other.html">Other</a>
+				<a href="/my-website/links.html">Links</a>
 			</div>
 		</div>
 	`);
